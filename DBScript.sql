b05897622ce6999500e243d4c08eff61a874af6a
Michał Sokół <sokol.michal95@live.com>
2017-03-16 13:12:10 +0100

    DBScript + Insert pattern

---
+++ DBScript.sql
@@ -0,0 +1,75 @@
+﻿CREATE TABLE [dbo].[Grupy_kasjerow] (
+    [Id]    INT           NOT NULL,
+    [Grupa] VARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+CREATE TABLE [dbo].[Odbiorcy] (
+    [Id_odbiorcy] INT           NOT NULL,
+    [NIP]         VARCHAR (10)  NULL,
+    [PESEL]       VARCHAR (11)  NULL,
+    [Miejscowosc] VARCHAR (MAX) NOT NULL,
+    [Ulica]       VARCHAR (MAX) NOT NULL,
+    [Nr_budynku]  INT           NOT NULL,
+    [Nr_lokalu]   INT           NULL,
+    CONSTRAINT [PK_Odbiorcy] PRIMARY KEY CLUSTERED ([Id_odbiorcy] ASC)
+);
+CREATE TABLE [dbo].[Kasjerzy] (
+    [Nr_kasjera] INT           NOT NULL,
+    [Imie]       VARCHAR (MAX) NOT NULL,
+    [Nazwisko]   VARCHAR (MAX) NOT NULL,
+    [Grupa]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Nr_kasjera] ASC),
+    CONSTRAINT [FK_Kasjerzy_Grupy_kasjerow] FOREIGN KEY ([Grupa]) REFERENCES [dbo].[Grupy_kasjerow] ([Id])
+);
+CREATE TABLE [dbo].[Czas_pracy] (
+    [Id]           INT      NOT NULL,
+    [Nr_kasjera]   INT      NOT NULL,
+    [Data_wejscia] DATETIME NOT NULL,
+    [Data_wyjscia] DATETIME NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Czas_pracy_Kasjerzy] FOREIGN KEY ([Nr_kasjera]) REFERENCES [dbo].[Kasjerzy] ([Nr_kasjera])
+);
+CREATE TABLE [dbo].[Faktury] (
+    [Nr_faktury]       INT  NOT NULL,
+    [Nr_paragonu]      INT  NOT NULL,
+    [ID_odbiorcy]      INT  NOT NULL,
+    [Data_wystawienia] DATE NOT NULL,
+    PRIMARY KEY CLUSTERED ([Nr_faktury] ASC),
+    CONSTRAINT [FK_Faktury_Odbiorcy] FOREIGN KEY ([ID_odbiorcy]) REFERENCES [dbo].[Odbiorcy] ([Id_odbiorcy]),
+    CONSTRAINT [FK_Faktury_Paragony] FOREIGN KEY ([Nr_paragonu]) REFERENCES [dbo].[Paragony] ([Id])
+);
+
+CREATE TABLE [dbo].[Logowania] (
+    [Id]         INT      NOT NULL,
+    [Nr_kasy]    INT      NOT NULL,
+    [Data]       DATETIME NOT NULL,
+    [Nr_kasjera] INT      NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Logowania_Kasjerzy] FOREIGN KEY ([Nr_kasjera]) REFERENCES [dbo].[Kasjerzy] ([Nr_kasjera])
+);
+
+CREATE TABLE [dbo].[Paragony] (
+    [Id]                INT NOT NULL,
+    [Nr_paragonu]       INT NOT NULL,
+    [Id_towaru]         INT NOT NULL,
+    [Id_kasjera]        INT NOT NULL,
+    [Ilosc]             INT NOT NULL,
+    [Statusy_platnosci] INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Paragony_Kasjerzy] FOREIGN KEY ([Id_kasjera]) REFERENCES [dbo].[Kasjerzy] ([Nr_kasjera]),
+    CONSTRAINT [FK_Paragony_Statusy_platnosci] FOREIGN KEY ([Statusy_platnosci]) REFERENCES [dbo].[Statusy_platnosci] ([Id]),
+    CONSTRAINT [FK_Paragony_Towary] FOREIGN KEY ([Id_towaru]) REFERENCES [dbo].[Towary] ([Id])
+);
+CREATE TABLE [dbo].[Statusy_platnosci] (
+    [Id]       INT           NOT NULL,
+    [Platnosc] VARCHAR (MAX) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+CREATE TABLE [dbo].[Towary] (
+    [Id]           INT           NOT NULL,
+    [EAN]          VARCHAR (13)  NOT NULL,
+    [Nazwa_towaru] VARCHAR (MAX) NOT NULL,
+    [Wartosc]      INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Insert.sql
@@ -0,0 +1,29 @@
+﻿--Czas_pracy
+INSERT INTO [dbo].[Czas_pracy] ([Id], [Nr_kasjera], [Data_wejscia], [Data_wyjscia]) VALUES (NULL, NULL, NULL, NULL)
+--Faktury
+INSERT INTO [dbo].[Faktury] ([Nr_faktury], [Nr_paragonu], [ID_odbiorcy], [Data_wystawienia]) VALUES (NULL, NULL, NULL, NULL)
+--Grupy_kasjerow
+INSERT INTO [dbo].[Grupy_kasjerow] ([Id], [Grupa]) VALUES (NULL, NULL)
+--Kasjerzy
+INSERT INTO [dbo].[Kasjerzy] ([Nr_kasjera], [Imie], [Nazwisko], [Grupa]) VALUES (NULL, NULL, NULL, NULL)
+--Logowania
+INSERT INTO [dbo].[Logowania] ([Id], [Nr_kasy], [Data], [Nr_kasjera]) VALUES (NULL, NULL, NULL, NULL)
+--Odbiorcy
+INSERT INTO [dbo].[Odbiorcy] ([Id_odbiorcy], [NIP], [PESEL], [Miejscowosc], [Ulica], [Nr_budynku], [Nr_lokalu]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL)
+--Paragony
+INSERT INTO [dbo].[Paragony] ([Id], [Nr_paragonu], [Id_towaru], [Id_kasjera], [Ilosc], [Statusy_platnosci]) VALUES (NULL, NULL, NULL, NULL, NULL, NULL)
+--Statusy_platnosci
+INSERT INTO [dbo].[Statusy_platnosci] ([Id], [Platnosc]) VALUES (1, N'Gotówka')
+INSERT INTO [dbo].[Statusy_platnosci] ([Id], [Platnosc]) VALUES (2, N'Karta platnicza')
+INSERT INTO [dbo].[Statusy_platnosci] ([Id], [Platnosc]) VALUES (3, N'Platnosc opózniona')
+--Towary
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (1, N'5908543603137', N'a', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (2, N'5908543603144', N'b', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (3, N'5908543603151', N'c', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (4, N'5908543603168', N'd', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (5, N'5908543603175', N'e', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (6, N'5908543603182', N'f', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (7, N'5908543603199', N'g', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (8, N'5908543603205', N'h', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (9, N'5908543603212', N'i', 1)
+INSERT INTO [dbo].[Towary] ([Id], [EAN], [Nazwa_towaru], [Wartosc]) VALUES (10, N'5908543603229', N'j', 1)
